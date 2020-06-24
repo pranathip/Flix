9998a114353e57e0a2a73b42d8c000555755db88
Pranathi Peri <pranathiperi@fb.com>
2020-06-24 15:15:06 -0500

    video three

---
+++ Flix/MovieCell.h
@@ -0,0 +1,20 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by Pranathi Peri on 6/24/20.
+//  Copyright © 2020 Pranathi Peri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@end
+
+NS_ASSUME_NONNULL_END
