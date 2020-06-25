183ed7ee5986dff8903ce0a3fa6568bf24e0110c
Pranathi Peri <pranathiperi@fb.com>
2020-06-25 14:24:13 -0500

    Added activityIndicator and conenctivity error message.

--- Flix/MoviesViewController.m
+++ Flix/MoviesViewController.m
@@ -17,6 +17,7 @@
 @property (nonatomic, strong) NSArray *movies;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -28,11 +29,14 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
+    //NSLog(@"activityIndicator Start");
+    [self.activityIndicator startAnimating];
     [self fetchMovies];
     
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
     [self.tableView insertSubview:self.refreshControl atIndex:0];
+
 }
 
 - (void) fetchMovies {
@@ -43,6 +47,26 @@
      NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
             if (error != nil) {
                 NSLog(@"%@", [error localizedDescription]);
+                //Error message
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline." preferredStyle:(UIAlertControllerStyleAlert)];
+                
+                // create a cancel action
+                UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                // handle cancel response here. Doing nothing will dismiss the view.
+                }];
+                // add the cancel action to the alertController
+                [alert addAction:cancelAction];
+
+                // create an OK action
+                UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                // handle response here.
+                }];
+                // add the OK action to the alert controller
+                [alert addAction:okAction];
+                
+                [self presentViewController:alert animated:YES completion:^{
+                    // optional code for what happens after the alert controller has finished presenting
+                }];
             }
             else {
                 NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
@@ -57,10 +81,12 @@
                 }
                 
                 [self.tableView reloadData];
+                [self.activityIndicator stopAnimating];
             }
          [self.refreshControl endRefreshing];
         }];
      [task resume];
+    //NSLog(@"activityIndicator Stop");
     
 }
 
